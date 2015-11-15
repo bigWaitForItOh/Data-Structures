f3d61eabdda8fd8d18265ad2b16a16fd4808f06b
Nishchal Dua <nishchaldua@gmail.com>
2015-11-15 19:45:04 +0530

    Update seg_tree_construct.py

--- segment_tree/python/seg_tree_construct.py
+++ segment_tree/python/seg_tree_construct.py
@@ -6,10 +6,8 @@ def seg_tree (array, pos):
 	l_child, r_child = ((pos + 1) * 2) - 1, (pos + 1) * 2;
 
 	if (array [l_child] == None):
-		print ('.');
 		seg_tree (array, l_child);
 	if (array [r_child] == None):
-		print ('.');
 		seg_tree (array, r_child);
 	array [pos] = array [l_child] + array [r_child];
 
