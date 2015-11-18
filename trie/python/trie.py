1afe05f81ae2beb216f29be532bd23bad9f4adf8
duaraghav8@gmail <duaraghav8@gmail.com>
2015-11-19 00:32:01 +0530

    Added values to strings

--- trie/python/trie.py
+++ trie/python/trie.py
@@ -1,3 +1,7 @@
+#################################################################################################################
+#Implementation of Trie Data Structure that supports insert ()-ing, search ()-ing and delete ()-ing a string
+#################################################################################################################
+
 class Node:
 	def __init__ (self):
 		self.children = {};
@@ -5,11 +9,17 @@ class Node:
 	def insert (self, key):
 		self.children [key] = Node ();
 
+	def insert_sentinal (self, value):
+		self.children [-1] = value;
+
 	def successors (self):
 		return (self.children);
 
 	def get_child (self, key):
 		return (self.children [key]);
+
+	def get_value (self):
+		return (self.children [-1] if -1 in self.children else None);
 
 class Trie:
 	def __init__ (self):
@@ -18,21 +28,26 @@ class Trie:
 	def get_root (self):
 		return (self.root);
 
-	def insert (self, current_node, string, start_pos = 0):
+	def insert (self, current_node, string, value, start_pos = 0):
 		if (start_pos == len (string)):
+			current_node.insert_sentinal (value);
 			return;
 
 		if (not (string [start_pos] in current_node.successors ())):
 			current_node.insert (string [start_pos]);
 
-		self.insert (current_node.get_child (string [start_pos]), string, start_pos + 1);
+		self.insert (current_node.get_child (string [start_pos]), string, value, start_pos + 1);
 
 	def search (self, current_node, string, start_pos = 0):
 		if (string [start_pos] in current_node.successors ()):
 			if (start_pos == len (string) - 1):
-				return (True);
+				return (current_node.get_child (string [start_pos]).get_value ());
 			return (self.search (current_node.get_child (string [start_pos]), string, start_pos + 1));
-		return (False);
+		return (None);
+
+	def delete (self, string):
+		#needs to be written
+		pass;
 
 	def describe (self):
 		#needs to be written
@@ -40,6 +55,10 @@ class Trie:
 
 if (__name__ ==  '__main__'):
 	myT = Trie ();
-	myT.insert (myT.get_root (), input ());
+	myT.insert (myT.get_root (), 'Stuxnet', 2010);
+	myT.insert (myT.get_root (), 'Shellshock', 2014);
+	myT.insert (myT.get_root (), 'Morris', 1988);
+	myT.insert (myT.get_root (), 'Heartbleed', 2014);
+
 	print (myT.search (myT.get_root (), input ()));
 #	myT.describe ();
